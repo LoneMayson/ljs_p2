6db697c001e48de78e28b5cd797350f38bde54da
LoneMayson <lone.mayson@gmail.com>
2021-09-06 10:46:15 +0500

    from spck

--- 001_011.js
+++ 001_011.js
@@ -1,2 +1,19 @@
 // coordinates
 let log = console.log
+
+function showMetrics()
+{
+    var elem = document.getElementById("elem");
+    var res = "";
+    var coords = elem.getBoundingClientRect();
+    res += "x: " + coords.x + "\n";
+    res += "y: " + coords.y + "\n";
+    res += "width: " + coords.width + "\n";
+    res += "height: " + coords.height + "\n";
+    res += "top: " + coords.top + "\n";
+    res += "left: " + coords.left + "\n";
+    res += "right: " + coords.right + "\n";
+    res += "bottom: " + coords.bottom + "\n";
+    var elem = document.getElementById("metrics");
+    elem.innerText = res;
+}
